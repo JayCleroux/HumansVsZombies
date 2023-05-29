ea10063da4aeb8b333294e728df8b7c929c5db7d
JayCleroux <w0270185@nscc.ca>
2023-05-29 17:09:30 -0300

    initial commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Jay/CLionProjects/JayCA4Zombies
+# Install script for directory: C:/Users/Jay/CLionProjects/ZombiesVsHumans
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/JayCA4Zombies")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/HumansVsZombies")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Jay/CLionProjects/JayCA4Zombies/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Jay/CLionProjects/ZombiesVsHumans/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
